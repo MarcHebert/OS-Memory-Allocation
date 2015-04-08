25bfcb288ae148e3024462e310ecf930ef452d2e
[MarcHebert] <[Marc.A.Hebert@gmail.com]>
2015-04-08 16:44:37 -0400

    heap block skeleton

--- memalloc.c
+++ memalloc.c
@@ -4,21 +4,87 @@
 #include <stdlib.h>
 #include <stdio.h>//for print debugger statements
 
+/*block structure
+
+if free block
+	[
+	+9		prevFreeBlock unsigned int (4 bytes)
+	+5 		nextFreeBlock unsigned int (4 bytes)
+	]
+else
+	+5		main content
+tagging scheme
+	+1		blockSize unsigned int (4 bytes)
+ blckaddr	isFree (1 if free, 0 otherwise) char (1 byte)
+	-1 			isPrevFree char	(1 byte)
+	-5 			prevBlockSize unsigned int (4 bytes)
+*/
+
+#define NAX_BLOCK_SIZE 128000 //bytes 128kb
+
+//boundary tag scheme
+#define SIZE_OFFSET 1
+#define FREE_OFFSET 0
+#define PREV_SIZE_OFFSET -5
+#define PREV_FREE_OFFSET -1
+#define TAG_SIZE 10
+
 //constants, bounds, bookkeeping etc.
-#define NAX_TOP_BLOCK_SIZE 128000 //bytes 128kb
+extern char *my_malloc_error;
+#define TRUE 1
+#define FALSE 0
+
+int init  = FALSE;
 unsigned long int topBlockSize = 0;
+unsigned int totalNumBytesAlloc = 0;
+unsigned int totalFreeSpace = NAX_TOP_BLOCK_SIZE;
+unsigned int largestContFreeSpace = NAX_TOP_BLOCK_SIZE;
+unsigned int numBlocks = 1;
+unsigned int numFreeBlocks = 1;
+
+unsigned int progBreak = 0;
+unsigned int progEnd = 0;
+void* headFreeBlock = NUll;
 
 //policy
-// 0 = first fit
-// 1 or anything else = best fit
+#define FIRST_FIT 0 
+#define BEST_FIT 1
+int policy = FIRST_FIT;// default is first fit
 
-int policy = 0;// default is first fit
+//accessor functions
 
 
 
-void *my_malloc(int size);
+int getBlockSize(void* bk)
+{
+	return *(int*) (bk + SIZE_OFFSET);
+}
 
-extern char *my_malloc_error;
+
+void print_BlockString(void* bk)
+{
+	printf("___---___BLOCKSTRING___---___\n");
+	printf("Addr: [%d]\n", bk);
+	printf("isFree")
+}
+
+
+void heap_init()
+{
+
+}
+
+void *my_malloc(int size)
+{
+	//scan list of free mem blocks
+	//choose one based on policy
+	//if appropriate is found
+	//TODO AFTERWORDS split chosen block
+	//else alloc another chunk of mem using sbrk()
+	//TODO AFTERWORDS split difference
+	//check if difference is beyond max and take appropriate action if so
+}
+
 
 void my_free(void *ptr);
 
